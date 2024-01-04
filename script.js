3736bea901d2df23def54fd693ee3d5829bfebfa
Prince Changani <princechangani56385638@gmail.com>
2024-01-04 16:57:44 +0530

    Add events for the model open and close
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = ()=>{
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModel = ()=>{
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for(let i =0; i < btnsOpenModal.length; i++)
+{
+    btnsOpenModal[i].addEventListener('click', openModel)
+}
+
+btnCloseModal.addEventListener('click', closeModel);
+
+overlay.addEventListener('click', closeModel);
